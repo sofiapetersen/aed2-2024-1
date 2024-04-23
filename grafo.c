c8e1322185bae589f3d726135a191dd9dab22cf9
Sofia Petersen <sppessil@inf.ufpel.edu.br>
2024-04-23 17:55:54 -0300

    algoritmo de dijkstra adicionado

--- grafo.c
+++ grafo.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+#define MAX_NODES 20
 
 struct Graph {
     int **pointer;
@@ -11,6 +14,9 @@ struct Graph {
 graph createGraph(int nodes);
 void addWeight(graph g, int src, int dest, int weight);
 void printGraphMatrix(graph g);
+void dijkstra(graph g, int src, int dest);
+int minDistance(int distance[], int visited[], int nodes);
+void printShortestDistances(int distance[], int src, int dest);
 void freeGraph(graph g);
 
 int main () {
@@ -42,6 +48,19 @@ int main () {
     }
 
     printGraphMatrix(g);
+
+    while(1){
+    int src_node, dest_node;
+    printf("\nDijkstra Algorithm\nEnter the source node (or -1 to end): ");
+    scanf("%d", &src_node);
+    if (src_node == -1)
+            break;
+    printf("Enter the destination node: ");
+    scanf("%d", &dest_node);
+
+    dijkstra(g, src_node, dest_node);
+    }
+
     freeGraph(g);
 
     return 0;
@@ -114,3 +133,47 @@ void freeGraph(graph g) {
     }
 }
 
+
+void dijkstra(graph g, int src, int dest) {
+    int distance[MAX_NODES];
+    int visited[MAX_NODES] = {0};
+
+    for (int i = 0; i < g->nodes; i++) {
+        distance[i] = INT_MAX;
+    }
+    distance[src] = 0;
+
+    for (int count = 0; count < g->nodes - 1; count++) {
+        int u = minDistance(distance, visited, g->nodes);
+        visited[u] = 1;
+
+        if (u == dest) // Se o nó de destino foi alcançado, interrompe a busca
+            break;
+
+        for (int v = 0; v < g->nodes; v++) {
+            if (!visited[v] && g->pointer[u][v] && distance[u] != INT_MAX && distance[u] + g->weight[u][v] < distance[v]) {
+                distance[v] = distance[u] + g->weight[u][v];
+            }
+        }
+    }
+
+    printShortestDistances(distance, src, dest);
+}
+
+
+int minDistance(int distance[], int visited[], int nodes) {
+    int min = INT_MAX, min_index;
+
+    for (int v = 0; v < nodes; v++) {
+        if (!visited[v] && distance[v] <= min) {
+            min = distance[v];
+            min_index = v;
+        }
+    }
+
+    return min_index;
+}
+
+void printShortestDistances(int distance[], int src, int dest) {
+    printf("\nShortest distance from node %d to node %d: %d\n", src, dest, distance[dest]);
+}
